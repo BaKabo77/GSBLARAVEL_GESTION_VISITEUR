27f2cba2f436e02888b95b39c4a658fcf771e197
BaKabo77 <157366052+BaKabo77@users.noreply.github.com>
2025-03-25 15:02:29 +0100

    Update sommairegestionnaire.blade.php

--- resources/views/sommairegestionnaire.blade.php
+++ resources/views/sommairegestionnaire.blade.php
@@ -3,21 +3,28 @@
             <!-- Division pour le sommaire -->
         <div id="menuGauche">
             <div id="infosUtil">
-                  
-             </div>  
+
+            </div>  
                <ul id="menuList">
                    <li >
                     <strong>Bonjour Gestionnaire : {{ $visiteur['nom'] . ' ' . $visiteur['prenom'] }}</strong>
                       
                    </li>
                   <li class="smenu">
-                     <a href="{{ route('chemin_listeVisiteur')}}" title="Liste des visiteurs ">Liste des visiteurs</a>
+                     <a href="{{ route('chemin_gestionFrais')}}" title="Saisie fiche de frais ">Saisie fiche de frais</a>
                   </li>
-                  
                   <li class="smenu">
+                    <a href="{{ route('chemin_listeVisiteur') }}" title="Consultation de la liste des visiteurs">Liste des visiteurs</a>
+                  </li>
+                  <li class="smenu">
+                    <a href="{{ route('chemin_selectionMois') }}" title="Consultation de mes fiches de frais">Mes fiches de frais</a>
+                  </li>
+               <li class="smenu">
                 <a href="{{ route('chemin_deconnexion') }}" title="Se déconnecter">Déconnexion</a>
                   </li>
                 </ul>
                
-        </div>
+            </div>
+                  
+             
     @endsection          
